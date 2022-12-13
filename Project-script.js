fc9763823730ca52288ac6a05d4bdd942158b200
Rozhin Alex <alexey.rozhin90@gmail.com>
2022-12-13 13:41:17 +0300

    1

---
+++ Project-script.js
@@ -0,0 +1,31 @@
+//Форма обратной связи
+
+function showModalWin() {
+
+    var darkLayer = document.createElement('div'); // слой затемнения
+    darkLayer.id = 'shadow'; // id чтобы подхватить стиль
+    document.body.appendChild(darkLayer); // включаем затемнение
+
+    var modalWin = document.getElementById('popupWin'); // находим наше "окно"
+    modalWin.style.display = 'flex'; // "включаем" его
+
+    darkLayer.onclick = function () {  // при клике на слой затемнения все исчезнет
+        darkLayer.parentNode.removeChild(darkLayer); // удаляем затемнение
+        modalWin.style.display = 'none'; // делаем окно невидимым
+        return false;
+    };
+    
+}
+//Выпадающее меню
+
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+    window.onclick = function(e) {
+    if (!e.target.matches('.menu-bar_dropdown')) {
+    var myDropdown = document.getElementById("myDropdown");
+      if (myDropdown.classList.contains('show')) {
+        myDropdown.classList.remove('show');
+      }
+    }
+  }
